d429164b86410ea59c54379fea23e144348b5246
alexander <starchenckoal@yandex.ru>
2024-02-18 22:02:46 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,171 @@
+import { useState } from "react"
+import { findBestMove,calculateWinner,checkDraw } from "./utils"
+
+
+type SquareProps = {
+  value:number
+  onClick:()=>void,
+  fraction:number
+}
+
+const Square:React.FC<SquareProps> = ({value,onClick,fraction}) => {
+  return <button onClick={onClick} className={`${fraction==20?'w-20 h-20':fraction==15?'w-15 h-15':'w-12 h-12'} border border-cyan-200`}>{value}</button>
+}
+const Board:React.FC = () => {
+  const [squares,setSquares] = useState(Array(9).fill(''))
+  const [xIsNext,setXIsNext] = useState(true)
+  const [playerChar,setPlayerChar] = useState('X')
+  const handleClick = (i:number) => {
+    const next = xIsNext? 'X':'O'
+    const newSquares = squares.slice()
+    if (calculateWinner(newSquares) || newSquares[i]) return;
+    newSquares[i] = next
+    setSquares(newSquares)
+    setXIsNext(!xIsNext)
+  }
+
+  const renderSquare = (i:number) => {
+    const fraction = squares.length == 9? 20: squares.length == 16? 15: 12
+    console.log(fraction)
+    return (
+      <Square value={squares[i]} fraction={fraction} onClick={()=>handleClick(i)}/>
+    )
+  }
+  
+  const winner = calculateWinner(squares)
+  let status = ''
+  if (winner){
+    status = 'Winner: ' + winner
+  } else if (checkDraw(squares)) {
+    status = 'Draw'
+    
+  }
+  if (!winner && !xIsNext && playerChar == 'X') {
+    const botIndex = findBestMove(squares,'O')
+    handleClick(botIndex)
+  }
+  if (playerChar != 'X' && xIsNext && !winner) {
+    const botIndex = findBestMove(squares,'X')
+    handleClick(botIndex)
+  }
+  const setBoard = (n:number) => {
+    setSquares(Array(n).fill(''))
+    setXIsNext(true)
+  }
+  return (
+    <div className="mx-auto">
+      <div className="w-60 mb-4">
+        <button onClick={()=>setBoard(9)} className="w-20 p-1 border border-black dark:border-white bg-black text-white hover:bg-white hover:text-black transition-all">3x3</button>
+        <button onClick={()=>setBoard(16)} className="w-20 p-1 border border-black dark:border-white bg-black text-white hover:bg-white hover:text-black transition-all">4x4</button>
+        <button onClick={()=>setBoard(25)} className="w-20 p-1 border border-black dark:border-white bg-black text-white hover:bg-white hover:text-black transition-all">5x5</button>
+      </div>
+      <div className="w-60 mb-2">
+        <label>You are playing as: </label>
+        <select onChange={(e)=>{setPlayerChar(e.target.value)
+        setSquares(Array(9).fill(''))
+        setXIsNext(true)}} className="text-black ml-1">
+          <option value={'X'}>X</option>
+          <option value={'O'}>O</option>
+        </select>
+      </div>
+      <div>{status}</div>
+      {
+        squares.length==9? <div>
+          <div className="flex flex-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="flex flex-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="flex flex-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+        </div> : squares.length == 16 ? <div>
+        <div className="flex flex-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+        {renderSquare(3)}
+      </div>
+      <div className="flex flex-row">
+        {renderSquare(4)}
+        {renderSquare(5)}
+        {renderSquare(6)}
+        {renderSquare(7)}
+      </div>
+      <div className="flex flex-row">
+        {renderSquare(8)}
+        {renderSquare(9)}
+        {renderSquare(10)}
+        {renderSquare(11)}
+      </div>
+      <div className="flex flex-row">
+        {renderSquare(12)}
+        {renderSquare(13)}
+        {renderSquare(14)}
+        {renderSquare(15)}
+      </div>
+        </div> : <div>
+        <div className="flex flex-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+        {renderSquare(3)}
+        {renderSquare(4)}
+      </div>
+      <div className="flex flex-row">
+        {renderSquare(5)}
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+        {renderSquare(9)}
+      </div>
+      <div className="flex flex-row">
+        {renderSquare(10)}
+        {renderSquare(11)}
+        {renderSquare(12)}
+        {renderSquare(13)}
+        {renderSquare(14)}
+      </div>
+      <div className="flex flex-row">
+        {renderSquare(15)}
+        {renderSquare(16)}
+        {renderSquare(17)}
+        {renderSquare(18)}
+        {renderSquare(19)}
+      </div>
+      <div className="flex flex-row">
+        {renderSquare(20)}
+        {renderSquare(21)}
+        {renderSquare(22)}
+        {renderSquare(23)}
+        {renderSquare(24)}
+      </div>
+        </div>
+      }
+        
+      
+      <button onClick={()=>{setSquares(prev=>Array(prev.length).fill(''))
+    setXIsNext(true)}} className="bg-white p-1 w-60 mt-5 dark:bg-black border border-solid border-black dark:border-white hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black transition-all">Reset</button>
+    </div>
+  )
+}
+
+
+const App:React.FC = () => {
+  return (
+    <div className="dark:bg-black flex flex-col items-center justify-center dark:text-white w-screen h-screen text-center">
+      <Board/>
+    </div>
+  )
+}
+
+
+
+export default App
---
+++ src/utils.ts
@@ -0,0 +1,86 @@
+import { chooseCombination } from "./winCombinations";
+
+function minimax(board:string[],  isMaximizing:boolean,pov:string,alpha:number,beta:number,depth:number) {
+  if (depth == 3) return 10
+  const scoreWinner = evaluate(board,pov);
+  if (scoreWinner == 10 || scoreWinner == -10) {
+    return scoreWinner
+  }
+  if (checkDraw(board)) {
+    return 0
+  }
+
+  if (isMaximizing) {
+    let bestScore = -Infinity;
+    for (let i = 0; i < board.length; i++) {
+      
+        if (board[i] == '') {
+          board[i] = pov;
+          bestScore = Math.max(bestScore,minimax(board,  !isMaximizing,pov,alpha,beta,depth+1))
+          alpha = Math.max(alpha,bestScore)
+
+          board[i] = '';
+          if (beta <= alpha) break;
+        }
+      
+    }
+    return bestScore;
+  } else {
+    let bestScore = Infinity;
+    for (let i = 0; i < board.length; i++) {
+      
+        if (board[i] == '') {
+          board[i] = pov == 'X'?'O':'X';
+         bestScore =Math.min(bestScore,minimax(board,!isMaximizing,pov,alpha,beta,depth))
+         beta = Math.min(beta,bestScore)
+          board[i] = '';
+          if (beta <= alpha) break;
+        }
+      
+    }
+    return bestScore;
+  }
+}
+  export function findBestMove(board:string[],pov:string):number {
+    let best = -Infinity
+    let bestMove = -1
+    for (let i=0; i<board.length;i++) {
+      if (board[i]==''){
+        board[i] = pov
+        const move = minimax(board,false,pov,-Infinity,+Infinity,0)
+        board[i] = ''
+
+        if (move > best) {
+          bestMove = i
+          best = move
+        }
+      }
+    }
+    return bestMove
+  }
+  export function checkDraw(board:string[]):boolean {
+    for (const el of board) {
+      if (!el) return false
+    }
+    return true
+  }
+
+
+export function calculateWinner(squares:string[]) : string {
+    const lines  = chooseCombination(squares.length)
+    for (const line of lines) {
+      let k=1
+      for (let i=0; i<line.length-1;i++) {
+        if (squares[line[i]] !== squares[line[i+1]]) break;
+        k++
+      }
+      if (k == line.length && squares[line[0]]) return squares[line[0]]
+    }
+    return ''
+  }
+function evaluate(board:string[],pov:string):number {
+    const winner = calculateWinner(board)
+    if (winner == pov) return 10
+    else if (winner !== '') return -10
+    else return 0
+  }
